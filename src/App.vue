88c829dc6983b1bdbbddc082cb20d4923f678d28
HRJHL <125470712+HRJHL@users.noreply.github.com>
2024-07-08 16:12:44 +0900

    11
    fin

---
+++ src/App.vue
@@ -0,0 +1,114 @@
+<!-- src/App.vue -->
+<template>
+  <div id="app" class="rol">
+    <header class="header">
+      <nav class="nav">
+        <router-link to="/" class="logo">
+          <img src="./assets/logo.png" alt="Logo" />
+        </router-link>
+        <div class="nav-links">
+          <router-link to="/message">메시지</router-link>
+          <router-link to="/credit">크레딧</router-link>
+        </div>
+      </nav>
+    </header>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+.header {
+  padding:10px 20px;
+  background-color: white;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 10000;
+  border-bottom: 1px solid #84848495;
+  overflow-x: hidden;
+}
+
+.nav {
+  display: flex;
+  align-items: center;
+  width: 100%;
+}
+
+.logo {
+  margin-left: 10%;
+  margin-right: 10%;
+  height: 30px;
+}
+
+.logo img {
+  height: 30px;
+}
+
+.nav-links {
+  display: flex;
+  gap: 100px;
+}
+.nav-links a {
+  color: black;
+  text-decoration: none;
+  font-size: 16px;
+}
+
+.nav-links a.router-link-active {
+  position: relative; 
+  color: inherit;
+}
+
+.nav-links a.router-link-active::after {
+  content: ''; 
+  position: absolute; 
+  left: -30px;
+  bottom: -24px; 
+  width: 260%; 
+  height: 4px; 
+  background-color: blue;
+}
+
+
+@media (max-width: 768px) {
+  html,body{
+    overflow-x: hidden;
+    width: 100%;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+
+  .header {
+    position: fixed;
+    z-index: 1000;
+    background-color: white;
+    height: 70px;
+    align-items: center;
+    border-bottom: 1px solid #84848495;
+  }
+
+.nav-links {
+  display: flex;
+  gap: 70px;
+}
+
+.logo {
+  margin-left: 5%;
+  margin-right: 10%;
+  height: 30px;
+}
+}
+
+</style>
---
+++ src/views/layout/MainHeader.vue
@@ -0,0 +1,113 @@
+<template>
+  <header class="header main-header">
+    <div id="app">
+    <header class="header">
+    <nav class="nav">
+        <router-link to="/" class="logo">
+          <img src="@/assets/logo2.svg" alt="Logo" />
+        </router-link>
+        <div class="nav-links">
+          <router-link to="/message" class ="mess">메시지</router-link>
+          <router-link to="/credit" class ="credit">크레딧</router-link>
+        </div>
+      </nav>
+  </header>
+    <router-view />
+  </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'MainHeader'
+};
+</script>
+
+<style scoped>
+.body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+}
+
+.header {
+  padding: 10px 20px;
+  background-color: black;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 100000;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.nav {
+  display: flex;
+  align-items: center;
+  width: 100%;
+}
+
+.logo {
+  margin-left: 10%;
+  margin-right: 10%;
+  height: 30px;
+}
+
+.logo img {
+  height: 30px;
+}
+
+
+.nav-links {
+  display: flex;
+  gap: 100px;
+}
+
+.nav-links a {
+  color: white;
+  text-decoration: none;
+  font-size: 16px;
+}
+
+.nav-links a:hover {
+  color: white;
+}
+
+@media (max-width: 480px) {
+  body{
+    overflow: hidden;
+    width: 100%;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+  .header {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 10000;
+    background-color: black;
+    widows: 480px;
+    height: 70px;
+    display: flex;
+    align-items: center;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    border-bottom: 1px solid #84848495;
+  }
+
+.nav-links {
+  display: flex;
+  gap: 70px;
+}
+
+.logo {
+  margin-left: 5%;
+  margin-right: 10%;
+  height: 30px;
+}
+}
+</style>
+
